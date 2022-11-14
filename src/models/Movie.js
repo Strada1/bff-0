886c0525781fd1f3ce0755d7bbce27380fb7e778
webcrg <webcrg@yandex.ru>
2022-11-14 17:56:25 +0300

    feat: add ref

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,7 +2,7 @@ const db = require('../ext/db');
 
 const MovieSchema = new db.Schema({
   title: String,
-  category: String,
+  category: { type: 'ObjectId', ref: 'Category' },
   year: Number,
   duration: Number,
   director: String,
