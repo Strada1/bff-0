6eb178dff081240c59a77e50eefadbaafa099916
somenave <gunichdrabl@gmail.com>
2022-11-14 14:22:48 +0300

    feat: #8 add category to Movie model

--- movies/src/models/Movie.js
+++ movies/src/models/Movie.js
@@ -5,6 +5,10 @@ const MovieSchema = new mongoose.Schema(
         title: String,
         year: Number,
         duration: Number,
+        category: {
+            type: 'ObjectId',
+            ref: 'categories'
+        }
     },
     {
         versionKey: false,
