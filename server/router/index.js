8703e74047feb4e640a6167106cc798ac66c54b6
Alexander Shvets <all.shvets.st@gmail.com>
2022-11-15 19:06:42 +0300

    Added router with get method

--- server/router/index.js
+++ server/router/index.js
@@ -2,13 +2,22 @@ const { Movie, Category } = require('../models/');
 
 module.exports = function (app) {
 
+  app.get('/', async (req, res) => {
+    try {
+      const movies = await Movie.find();
+      return res.status(200).send(movies);
+    } catch (err) {
+      return res.status(500).send('error');
+    }
+  });
+
   app.post('/movies', async (req, res) => {
     try {
-      const movie = await Movie.create(req.body);
+      const movie = await Movie.create(req.body); // создаем новый документ для модели Movie
       return res.status(201).send(movie);
     } catch (err) {
-      console.log(err);
-      return res.status(500).send('error');
+      console.log('Error:', err.message);
+      return res.status(500).send(err.message);
     }
   });
 
@@ -17,8 +26,8 @@ module.exports = function (app) {
       const category = await Category.create(req.body);
       return res.status(201).send(category);
     } catch (err) {
-      console.log(err);
-      return res.status(500).send('error');
+      console.log('Error:', err.message);
+      return res.status(500).send(err.message);
     }
   });
 
