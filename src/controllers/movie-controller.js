85303978058f279bf8ec166fd9f9e9498d99c1d2
Ilya Shishkin <mostbook@mail.ru>
2022-11-16 13:10:56 +0700

    Added comments in movie schema Added routes and controllers to create and delete comments

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -8,27 +8,51 @@ class Controller {
 
     return res.status(201).send(movie);
   };
-
   get = async ( req, res ) => {
     const { movieId } = req.params;
     const movie = await Movie.findById(movieId);
 
     return res.status(200).send(movie);
   };
-
   update = async ( req, res ) => {
     const { movieId } = req.params;
     const newMovie = req.body;
-    const movie = await Movie.findByIdAndUpdate(movieId, newMovie, { new: true });
+    const movie = await Movie.findByIdAndUpdate(
+      movieId,
+      newMovie,
+      { new: true },
+    );
 
     return res.status(200).send(movie);
   };
-
   delete = async ( req, res ) => {
     const { movieId } = req.params;
     await Movie.findByIdAndDelete(movieId);
 
     return res.status(200).send();
+  };
+  addComment = async ( req, res ) => {
+    const { movieId } = req.params;
+    const newComment = req.body;
+
+    const movie = await Movie.findByIdAndUpdate(
+      movieId,
+      { $push: { comments: newComment } },
+      { new: true },
+    );
+
+    return res.status(200).send(movie);
+  };
+  deleteComment = async ( req, res ) => {
+    const { movieId, commentId } = req.params;
+
+    const movie = await Movie.findByIdAndUpdate(
+      movieId,
+      { $pull: { comments: { _id: commentId } } },
+      { new: true },
+    );
+
+    return res.status(200).send(movie);
   };
 }
 
--- src/models/movie.js
+++ src/models/movie.js
@@ -3,10 +3,11 @@ const { ObjectId } = require('mongodb');
 
 const MovieSchema = new mongoose.Schema({
   title: String,
-  category: {type: ObjectId, ref: "Category"},
+  category: { type: ObjectId, ref: 'Category' },
   year: Number,
   duration: Number,
   director: String,
+  comments: [ { text: String } ],
 });
 
 const Movie = mongoose.model('Movie', MovieSchema);
