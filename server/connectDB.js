802bb6df92eb8bd8ade340b3c41741b7146aea16
Alexander Shvets <all.shvets.st@gmail.com>
2022-11-20 18:11:02 +0300

    Refactoring server.js

--- server/connectDB.js
+++ server/connectDB.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const URL = process.env.MONGO_CONNECTION_STRING;
 
-mongoose
+module.exports.connect = function () {
+ mongoose
   .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
   .catch((err) => console.error('Database error', err));
+}
