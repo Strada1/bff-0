5f23e98f7194b9e65cc3949636e1bd1fa1d90db6
Alexander Shvets <all.shvets.st@gmail.com>
2022-11-26 02:20:51 +0300

    Refactoring loading list

--- server/utils/loadingListToCollection.js
+++ server/utils/loadingListToCollection.js
@@ -3,21 +3,31 @@ import Movie from '../models/Movie.js';
 
 loadingListToCollection('../movies.json', Movie).then(console.log);
 
-async function loadingListToCollection(path, model) {
+export async function loadingListToCollection(path, model) {
   try {
     const file = await fs.readFile(path, { encoding: 'utf8' });
     const docs = JSON.parse(file);
     const loadingList = [];
 
+    let successCount = 0;
+    let errorCount = 0;
+    const map = new Map();
+
     for (let doc of docs) {
       const missingFields = validate(['title', 'year', 'duration'], doc);
 
       if (missingFields.length > 0) {
-        console.log(`missing fields: ${missingFields.join(', ')}`);
+        map.set(missingFields, doc);
+        errorCount++;
         continue;
       }
+
       loadingList.push(doc);
+      successCount++;
     }
+
+    console.log(`Success: ${successCount}, Errors: ${errorCount}`);
+    console.log('Map Errors: ', map);
 
     return await model.insertMany(loadingList);
   } catch (err) {
