2abcdaba2b64ebde814c019ae6ae6aa6252e3e78
Ilya Shishkin <mostbook@mail.ru>
2022-12-02 07:58:35 +0700

    Fixed the port output. It is now being read from .env file

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.use(cors({ origin: ALLOWED_ORIGINS }));
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`);
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
